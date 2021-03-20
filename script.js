986e11c30352de765fdc93acb57160b4a8ad78b1
Kaustubh patil <pkaustubh401@gmail.com>
2021-03-21 04:49:17 +0530

    fixed the copy bug

--- script.js
+++ script.js
@@ -6,19 +6,25 @@ var flkty = new Flickity( elem, {
 });
 
 
-document.getElementById('copy_button').onclick = function() {
-    /* Get the text field */
-    
-    var x=this.parentElement.parentElement.children[1];
-  
-    /* Select the text field */
-    x.select();
-    x.setSelectionRange(0, 99999); /* For mobile devices */
-  
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-  
-    /* Alert the copied text */
-    //alert("Copied the text: " + x.value);
-};
+var y=document.getElementsByClassName('btn');
+
+for(var i=0, len=y.length|0; i<len; i=i+1|0){
+    y[i].onclick = function() {
+        /* Get the text field */
+        
+        var x=this.parentElement.parentElement.children[1];
+      
+        /* Select the text field */
+        x.select();
+        x.setSelectionRange(0, 99999); /* For mobile devices */
+      
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+      
+        /* Alert the copied text */
+        alert("Copied the text: " + x.value);
+    };
+}
+
+
 
